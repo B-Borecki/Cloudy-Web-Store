514d8db671e821ac37b2c9ce7e1d2b312b9bc47b
B-Borecki <bb_orecki@outlook.com>
2024-01-06 00:59:34 +0100

    Created bootstrap navbar

--- src/server.js
+++ src/server.js
@@ -1,4 +1,25 @@
-const express = require("express")
-const app = express()
-app.set("view engine", "ejs")
+const express = require("express");
+const ejs = require('ejs');
+//const AWS = require('aws-sdk');
+const app = express();
+const port = 80;
+app.set("view engine", "ejs");
+app.use(express.static('views'));
+app.use(express.static('images'));
+
+app.get('/', (req, res) => {
+  res.render('index', { pageTitle: 'Cloudy Web Store' });
+});
+
+app.get('/login', (req, res) => {
+  res.render('login', { pageTitle: 'Sign in' });
+});
+
+app.get('/register', (req, res) => {
+  res.render('register', { pageTitle: 'Sign up' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
