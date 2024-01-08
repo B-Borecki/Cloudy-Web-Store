0ccbe57e7f6a393ef45043683fc61a3bd122ee77
B-Borecki <bb_orecki@outlook.com>
2024-01-08 01:30:40 +0100

    Configured connection to the database

--- src/server.js
+++ src/server.js
@@ -1,14 +1,42 @@
 const express = require("express");
 const ejs = require('ejs');
+const mysql = require('mysql');
 //const AWS = require('aws-sdk');
+
+const {db_endpoint, db_port, db_user, db_passwd, db, imgs_path} = require('./vars');
+
 const app = express();
 const port = 80;
 app.set("view engine", "ejs");
 app.use(express.static('views'));
 app.use(express.static('images'));
 
+const connection = mysql.createConnection({
+  host: db_endpoint,
+  user: db_user,
+  password: db_passwd,
+  database: db
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Błąd połączenia z bazą danych: ' + err.stack);
+    return;
+  }
+  console.log("Połączono z bazą danych");
+});
+
 app.get('/', (req, res) => {
   res.render('index', { pageTitle: 'Cloudy Web Store' });
+
+  const query = 'SELECT * FROM products';
+  connection.query(query, (error, results, fields) => {
+    if (error) {
+      console.error('Błąd zapytania: ' + error.stack);
+      return;
+    }
+    console.log(results);
+  });
 });
 
 app.get('/login', (req, res) => {
