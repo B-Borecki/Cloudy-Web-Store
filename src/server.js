2aef08b1a0883d5c576b9af1d53b0e9bafeec3da
B-Borecki <bb_orecki@outlook.com>
2024-01-16 23:09:25 +0100

    Added product page

--- src/server.js
+++ src/server.js
@@ -62,6 +62,20 @@ app.get('/search', (req, res) => {
 });
 
 
+app.get('/products/:ami', (req, res) => {
+  ami_id = req.params.ami;
+  const query = 'SELECT * FROM products WHERE ami_id = "' + ami_id + '";';
+    connection.query(query, (error, results, fields) => {
+    if (error) {
+      console.error('Błąd zapytania: ' + error.stack);
+      return;
+    }
+    product = results[0];
+    res.render('product', {pageTitle: product.name, product : product, imgs_path: imgs_path});
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
